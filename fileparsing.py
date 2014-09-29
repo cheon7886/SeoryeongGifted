4d3125c606e38e274204e3720b426b4990f66bd0
Minseok Jeon <cheon7886@naver.com>
2014-09-29 12:45:01 +0900

    Added Function, Added Option Parsing

--- fileparsing.py
+++ fileparsing.py
@@ -1,36 +1,45 @@
 # -*- coding: utf-8 -*-
 
+import optparse
 import fileinput
 import sys
 
+parser = optparse.OptionParser("usage%prog -c <countername> -d <directory>")
+parser.add_option('-c', dest='counter', type='string',help='notify me about talking counter')
+parser.add_option('-d', dest='directory', type='string', help='notify me about log path')
+(options, args) = parser.parse_args()
+
+cname = options.counter
+dname = options.directory
+
 def replaceKAKAO(file):
     for line in fileinput.input(file, inplace=1):
-	if "년 " in line:
-	    line = line.replace("년 ", ",")
-	if "월 " in line:
-	    line = line.replace("월 ", ",")
-	if "일 " in line:
-	    line = line.replace("일 ", ",")
-	if "오후 " in line:
+        if "년 " in line:
+            line = line.replace("년 ", ",")
+        if "월 " in line:
+            line = line.replace("월 ", ",")
+        if "일 " in line:
+            line = line.replace("일 ", ",")
+        if "오후 " in line:
             line = line.replace("오후 ", ",")
-	if ":" in line:
-	    line = line.replace(":", ",")
-	if " 회원님 " in line:
-	    line = line.replace(" 회원님 ", "me")
-	if " 보기드문남자♥ " in line:
-	    line = line.replace(" 보기드문남자♥ ", "counter")
+        if ":" in line:
+            line = line.replace(":", ",")
+        if " 회원님 " in line:
+            line = line.replace(" 회원님 ", "me")
+        if counter in line:
+            line = line.replace(cname, "counter")
 
-	sys.stdout.write(line)
+        sys.stdout.write(line)
 
 def remove_12line(file):
     for line_number, line in enumerate(fileinput.input(file, inplace=1)):
-	if line_number == 0:
-	    continue
-	if line_number == 1:
-	    continue
-	else:
-	    sys.stdout.write(line)
+        if line_number == 0:
+            continue
+        if line_number == 1:
+            continue
+        else:
+            sys.stdout.write(line)
 
-remove_12line("Downloads/KakaoTalkChats.txt")
-replaceKAKAO("Downloads/KakaoTalkChats.txt")
+remove_12line(dname)
+replaceKAKAO(dname)
 
