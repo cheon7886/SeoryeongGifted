cfff85cb29cc94754df282984bbccf28900d06c9
Minseok Jeon <cheon7886@naver.com>
2014-10-01 00:11:27 +0900

    Features added, added CSV output feature, bug fixed

--- fileparsing.py
+++ fileparsing.py
@@ -3,14 +3,23 @@
 import optparse
 import fileinput
 import sys
+import csv
 
-parser = optparse.OptionParser("usage%prog -c <countername> -d <directory>")
+parser = optparse.OptionParser("usage%prog -c <countername> -d <directory> -o <outputfile>")
 parser.add_option('-c', dest='counter', type='string',help='notify me about talking counter')
 parser.add_option('-d', dest='directory', type='string', help='notify me about log path')
+parser.add_option('-o', dest='outputfile', type='string', help='specify about outputcsv file name')
+
 (options, args) = parser.parse_args()
 
 cname = options.counter
 dname = options.directory
+fname = options.outputfile
+
+def convertcsv(in_file,out_file):
+    fin_txt = csv.reader(open(in_file, "rb"), delimiter = ',')
+    out_csv = csv.writer(open(out_file, 'wb'))
+    out_csv.writerows(fin_txt)
 
 def replaceKAKAO(file):
     for line in fileinput.input(file, inplace=1):
@@ -20,16 +29,22 @@ def replaceKAKAO(file):
             line = line.replace("월 ", ",")
         if "일 " in line:
             line = line.replace("일 ", ",")
+        if "오전 " in line:
+            line = line.replace("오전 ", "morning,")
         if "오후 " in line:
-            line = line.replace("오후 ", ",")
+            line = line.replace("오후 ", "afternoon,")
         if ":" in line:
             line = line.replace(":", ",")
         if " 회원님 " in line:
             line = line.replace(" 회원님 ", "me")
-        if counter in line:
+        if cname in line:
             line = line.replace(cname, "counter")
-
-        sys.stdout.write(line)
+        if ", " in line:
+            line = line.replace(", ", ",")
+        else:
+            print "Countername was not found"
+            exit(0)
+            sys.stdout.write(line)
 
 def remove_12line(file):
     for line_number, line in enumerate(fileinput.input(file, inplace=1)):
@@ -37,9 +52,17 @@ def remove_12line(file):
             continue
         if line_number == 1:
             continue
+        if line_number == 2:
+            continue
+        if line_number == 3:
+            continue
         else:
             sys.stdout.write(line)
 
-remove_12line(dname)
-replaceKAKAO(dname)
-
+if (cname == None) | (dname == None) | (fname == None):
+    print parser.usage
+    exit(0)
+else :
+    remove_12line(dname)
+    replaceKAKAO(dname)
+    convertcsv(dname,fname)
